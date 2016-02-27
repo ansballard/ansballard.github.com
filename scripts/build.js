234eb683c233cd6e9309a517bbe367fa723aa5a9
ansballard <ans.ballard@gmail.com>
2016-02-27 08:00:27 -0600

    Moved assets to src, built to dist with postcss and uncss, minified index.html

--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,22 @@
-#!/usr/bin/node
-
 const marked = require("marked");
+const htmlmin = require("html-minifier").minify;
 const Promise = require("bluebird");
 var fs = Promise.promisifyAll(require("fs"));
 
+const minOptions = {
+  removeComments: true,
+  collapseWhitespace: true
+};
+
 Promise.all([
-  fs.readFileAsync("./partials/header.html", "utf8"),
-  fs.readFileAsync("./md/index.md", "utf8").then(md => `\n<!-- transpiled from index.md -->\n${marked(md)}`),
-  fs.readFileAsync("./partials/footer.html", "utf8")
-]).then(files => {
-  return fs.writeFileAsync("./index.html", files.join(""))
+  fs.readFileAsync("./src/partials/header.html", "utf8"),
+  fs.readFileAsync("./src/md/index.md", "utf8").then(md => `\n<!-- transpiled from index.md -->\n${marked(md)}`),
+  fs.readFileAsync("./src/partials/footer.html", "utf8")
+])
+.then(files => files.join(""))
+.then(html => htmlmin(html, minOptions))
+.then(html => {
+  return fs.writeFileAsync("./index.html", html)
 }).then(() => {
   console.log("index.html Built");
 }).catch(e => {
