929c664877a2c7f82c3006742d8400e24c8bd665
ansballard <ans.ballard@gmail.com>
2016-03-02 18:01:03 -0600

    Added spec script, runs on postdeploy, generates  with a table of CSS compression percentages

--- scripts/specs.js
+++ scripts/specs.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
-const browserify = require("browserify");
-const babelify = require("babelify");
-const uglifyjs = require("uglify-js");
+"use strict";
 
 const postcss = require("postcss");
 const cssImport = require("postcss-import");
@@ -12,46 +9,50 @@ const cssnano = require("cssnano");
 
 const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
-//
-// module.exports = function Specs() {
-// 	return new Promise((resolve, reject) => {
-    fs.readFileAsync("./src/css/entry.css", "utf8")
-    .then(css => {
-      postcss([cssImport, cssnext])
-  			.process(css, {
-  				from: "./src/css/entry.css",
-  				to: "spec.css"
-  			})
-  			.then(result => {
-          console.log(`CSS Unminified: ${result.css.length} bytes`);
-  				return result.css;
-  			})
-        .then(css => {
-          return new Promise((resolve, reject) => {
-            uncss(["./index.html"], {
-              raw: css
-            }, (err, output) => {
-              if(err) {
-                reject(err);
-              } else {
-                console.log(`CSS after uncss: ${output.length} bytes (${Math.floor(output.length/css.length * 100)}%)`);
-                resolve(output);
-              }
-            });
-          });
-        })
-        .then(css => {
-          return cssnano.process(css)
-          .then(result => {
-            console.log(`CSS after cssnano: ${result.css.length} bytes (${Math.floor(result.css.length/css.length * 100)}%)`);
-            return result.css;
-          });
-        })
-        .catch(e => {
-          console.log(e);
-          return e;
-        });
-    });
 
-	// });
-// }
+let mdOutput = "## CSS Compression Specs\r\n\r\n|Step|Size in Bytes|Compression %|\r\n|---|---|---|\r\n";
+let largestTotal;
+
+fs.readFileAsync("./src/css/entry.css", "utf8")
+	.then(css => {
+		postcss([cssImport, cssnext])
+			.process(css, {
+				from: "./src/css/entry.css",
+				to: "spec.css"
+			})
+			.then(result => {
+        largestTotal = result.css.length;
+				console.log(`CSS Unminified: ${result.css.length} bytes (100%)`);
+        mdOutput += `|CSS Unminified|${result.css.length} bytes|100%|\r\n`;
+				return result.css;
+			})
+			.then(css => {
+				return new Promise((resolve, reject) => {
+					uncss(["./index.html"], {
+						raw: css
+					}, (err, output) => {
+						if (err) {
+							reject(err);
+						} else {
+							console.log(`CSS after uncss: ${output.length} bytes (${Math.floor(output.length/largestTotal * 100)}%)`);
+              mdOutput += `|After UnCSS|${output.length} bytes|${Math.floor(output.length/largestTotal * 100)}%|\r\n`;
+							resolve(output);
+						}
+					});
+				});
+			})
+			.then(css => {
+				return cssnano.process(css)
+					.then(result => {
+						console.log(`CSS after cssnano: ${result.css.length} bytes (${Math.floor(result.css.length/largestTotal * 100)}%)`);
+            mdOutput += `|After cssnano|${result.css.length} bytes|${Math.floor(result.css.length/largestTotal * 100)}%|`;
+						return result.css;
+					});
+			})
+      .then(css => {
+        return fs.writeFileAsync("./compressionSpecs.md", mdOutput, "utf8");
+      })
+      .catch(e => {
+        console.log("Error:", e);
+      });
+	});
